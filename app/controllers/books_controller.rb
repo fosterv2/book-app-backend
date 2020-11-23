48345a2d1d2656f5ada35b2878927fb3e4d61af8
fosterv2 <fosterv2@outlook.com>
2020-11-23 15:15:10 -0800

    Add reviews and make many-to-many relationship

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,15 @@
 class BooksController < ApplicationController
+    def index
+        books = Book.all
+        render json: books.to_json(except: [:created_at, :updated_at])
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    # def destroy
+    # end
 end
