0ee138b1d8d893fefe07c89865df7d0560ca7d70
fosterv2 <fosterv2@outlook.com>
2020-11-23 14:59:03 -0800

    Seed db with some books

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+api_data = RestClient.get('https://www.googleapis.com/books/v1/volumes?q=bundo')
+book_data = JSON.parse(api_data)["items"]
+
+10.times do
+    book = book_data.shift["volumeInfo"]
+    Book.create(
+        name: book["title"],
+        author_name: book["authors"] ? book["authors"][0] : "None",
+        img_url: book["imageLinks"]["thumbnail"]
+    )
+end
