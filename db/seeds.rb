7267e0fa6a10478db92b531dc0650353016def9b
fosterv2 <fosterv2@outlook.com>
2020-12-12 13:33:52 -0800

    Seed more books and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-api_data = RestClient.get('https://www.googleapis.com/books/v1/volumes?q=inauthor:madeleine+l%27engle')
-book_data = JSON.parse(api_data)["items"]
+api_data_1 = RestClient.get('https://www.googleapis.com/books/v1/volumes?q=inauthor:madeleine+l%27engle')
+book_data = JSON.parse(api_data_1)["items"]
+api_data_2 = RestClient.get('https://www.googleapis.com/books/v1/volumes?q=inauthor:rowling')
+book_data += JSON.parse(api_data_2)["items"]
+api_data_3 = RestClient.get('https://www.googleapis.com/books/v1/volumes?q=inauthor:seuss')
+book_data += JSON.parse(api_data_3)["items"]
 
 book_data.each do |bk|
     book = bk["volumeInfo"]
@@ -16,5 +20,14 @@ book_data.each do |bk|
         author_name: book["authors"] ? book["authors"][0] : "None",
         img_url: book["imageLinks"] ? book["imageLinks"]["thumbnail"] : "none",
         blurb: book["description"]
+    )
+end
+
+10.times do
+    name = Faker::Name.name
+    User.create(
+        name: name,
+        username: Faker::Internet.username(specifier: name, separators: %w(. _ -)),
+        password: Faker::Internet.password
     )
 end
